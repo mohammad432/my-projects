53ccd4c6403265c4d8ba4442f936cfc40ebe2d91
mohammad432 <49672373+mohammad432@users.noreply.github.com>
2019-06-16 09:50:13 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int arry_[] = {1,10,100,1000,10000,100000,1000000,80000000,9000000000,100000000,11000000};
+    for (int i=10;i>=0; i--)
+    {
+        cout << arry_[i] <<endl;
+    }
+
+
+    return 0;
+}
+
